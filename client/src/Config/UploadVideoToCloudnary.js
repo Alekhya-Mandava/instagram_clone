47447aa9769c053b7bcb2219f718eb6c4963f826
Alekhya Mandava <alekhya.mandava18@gmail.com>
2024-07-29 11:09:44 -0700

    Update UploadVideoToCloudnary.js
    commited random cloudinary key

--- client/src/Config/UploadVideoToCloudnary.js
+++ client/src/Config/UploadVideoToCloudnary.js
@@ -4,10 +4,10 @@ export const uploadMediaToCloudinary = async (media) => {
       data.append("file", media);
       data.append("resource_type", "video");
       data.append("upload_preset", "instagram_clone");
-      data.append("cloud_name", "dtjy0kci5");
+      data.append("cloud_name", "djhkxnjc");
   
       const res = await fetch(
-        "https://api.cloudinary.com/v1_1/dtjy0kci5/video/upload",
+        "https://api.cloudinary.com/v1_1/djhkxnjc/video/upload",
         {
           method: "post",
           body: data,
